aab932b2383df5b36de05e9e0dcbab69819e59b1
이석주 <nick981213@naver.com>
2025-01-12 18:23:59 +0900

    - 스크립트 파일 수정

--- todo-webhook-handler.sh
+++ todo-webhook-handler.sh
@@ -4,9 +4,9 @@ echo "Pulling the latest Docker image..."
 docker pull hknhj/todoapp:latest
 
 echo "Stopping and removing the old container..."
-docker stop todoapp || true && docker rm todoapp || true
+docker stop todoapp || true
 
 echo "Starting a new container..."
-docker run -d --name todoapp -p 3000:3000 hknhj/todoapp:latest
+docker run -d --rm --name todoapp --env-file /home/ubuntu/TodoAPP/.env -p 3000:3000 hknhj/todoapp:latest
 
 echo "Deployment complete."
